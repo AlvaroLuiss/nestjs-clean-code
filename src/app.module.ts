5ef77dff16f5b8c6326c635d6cc297a6162bcc64
alvaroluiss <alvaro.luiscosta@hotmail.com>
2024-07-07 14:56:59 -0300

    feat: list recents questions controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { envSchema } from 'src/env'
 import { AuthModule } from './auth/auth.module'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { AuthenticateController } from './controllers/authenticate.controller'
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
 
 @Module({
@@ -19,6 +20,7 @@ import { CreateQuestionController } from './controllers/create-question.controll
     CreateAccountController,
     AuthenticateController,
     CreateQuestionController,
+    FetchRecentQuestionsController,
   ],
   providers: [PrismaService],
 })
