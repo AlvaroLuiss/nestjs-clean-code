ab8fa3d209364a50bfc1aa3eeda7817ca504b32f
alvaroluiss <alvaro.luiscosta@hotmail.com>
2024-07-08 10:37:16 -0300

    feat: question mapper from prisma to domain

---
+++ src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -0,0 +1,27 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Question } from '@/domain/forum/enterprise/entities/question'
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
+import { Question as PrismaQuestion } from '@prisma/client'
+
+
+
+// Quero pegar uma question do prisma e converter ela para uma question  de domain.
+
+export class PrismaQuestionMapper {
+    static toDomain(raw: PrismaQuestion): Question {
+        return Question.create({
+            title: raw.title,
+            content: raw.content,
+            authorId: new UniqueEntityID(raw.authorId),
+            bestAnswerId: undefined,
+            slug: Slug.create(raw.slug),
+            createdAt: raw.createdAt,
+            updatedAt: raw.updatedAt,
+
+        }, new UniqueEntityID(raw.id))
+    }
+}
+
+
+// undefined - valor não existe
+// null - valor não preenchido
